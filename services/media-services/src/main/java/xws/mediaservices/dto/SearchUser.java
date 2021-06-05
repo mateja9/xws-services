c9632107356b006c108940c723bab17e461e99a8
Milica <milicamimamitrovic@gmail.com>
2021-06-06 01:51:20 +0200

    Pocetak pretrage profila

--- services/media-services/src/main/java/xws/mediaservices/dto/SearchUser.java
+++ services/media-services/src/main/java/xws/mediaservices/dto/SearchUser.java
@@ -1,6 +1,39 @@
 package xws.mediaservices.dto;
 
 public class SearchUser {
+    private String name;
+    private String lastname;
+    private String username;
 
+    public SearchUser(){ }
 
+    public SearchUser(String name, String lastname, String username){
+        this.name=name;
+        this.lastname=lastname;
+        this.username=username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
