875cdd7a467fda6d790eb7658ef3eb626d7df4e8
Milica <milicamimamitrovic@gmail.com>
2021-06-27 14:45:41 +0200

    Follow stranica + pretrage lokacija i tagova

--- APM/src/app/korisnik/profil-korisnika/profil-korisnika.module.ts
+++ APM/src/app/korisnik/profil-korisnika/profil-korisnika.module.ts
@@ -8,11 +8,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { ProfilNeRegKorComponent } from 'app/korisnik/profilNeRegKor/profilNeRegKor.component';
 import { ExploreNistagramComponent } from '../explore/exploreNistagram.component';
 import { KorisnikService } from 'app/services/korisnik.services';
+import { FollowComponent } from '../follow/follow.component';
 
 
 @NgModule({
   declarations: [ProfilKorisnikaComponent,
-    ExploreNistagramComponent
+    ExploreNistagramComponent,
+    FollowComponent
    ],
 
   imports: [
@@ -22,6 +24,7 @@ import { KorisnikService } from 'app/services/korisnik.services';
 
     RouterModule.forChild([
       { path: 'explore', component: ExploreNistagramComponent },
+      { path: 'explore/:id', component: ExploreNistagramComponent },
       { path: 'korisnik', component: ProfilKorisnikaComponent },
       { path: 'profilNeRegKor/:id', component: ProfilNeRegKorComponent },
       { path: 'profilNeRegKor', component: ProfilNeRegKorComponent },
